7e2cb5a63fe86658e12e6719c6c2e0fc4c337f75
Xander Beberman <xanderb@uchicago.edu>
2019-03-31 20:39:37 -0500

    first attempt at off button

--- src/server.py
+++ src/server.py
@@ -14,11 +14,14 @@ def index():
 def change_color():
 	error = None
 	if request.method == 'POST':
+		# tell any current running threads to stop
 		stopper.stop = True
+		# get color
 		form = request.get_json(force=True)
 		app.logger.debug(form)
 		color = form['color']
 		color = hex_to_rgb(color)
+		# wait until threads have actually stopped
 		sem.acquire()
 		show(color)
 		app.logger.debug(color)
@@ -36,6 +39,15 @@ def make_rainbow():
 		stopper.stop = False
 		rainbow(exit=stopper, sem=sem)
 	return render_template('index.html', current='Rainbow!!!')
+
+@app.route('/off.html', methos=['POST', 'GET'])
+def off():
+	stopper.stop = True
+	sem.acquire()
+	show((0, 0, 0))
+	app.logger.debug(color)
+	stopper.stop = False
+	sem.release()
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
